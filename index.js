5d9696548290d6e84d550b23c3a7ff63186fef63
Utsav <utsavsavani415@gmail.com>
2023-08-30 12:32:34 +0530

    mongoose setup

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 const express = require("express");
-
+require("./db/config");
+const User = require("./db/Users");
 const app = express();
 
+app.use(express.json())
 const port = 5000;
 
-app.get("/", (req, res) => res.send("Hello World!"));
+const connectDB = async () => {
+  const data = await User.find();
+
+  console.log(data);
+};
+
+connectDB();
+
+
+app.post("/register", (req, res) => {
+  res.send(req.body);
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
